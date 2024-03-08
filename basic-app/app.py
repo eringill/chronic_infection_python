a98273782c5bbb3e9715cdf00f28e41a9a4a895f
Erin Gill <erin.gill81@gmail.com>
2024-03-07 16:58:32 -0800

    feat: create basic app, add .tsv files

---
+++ basic-app/app.py
@@ -0,0 +1,11 @@
+from shiny.express import input, render, ui
+import plotly.express as px
+from shinywidgets import render_plotly
+
+ui.page_opts(title="SARS-CoV-2 Chronic Infection Calculator")
+
+with ui.sidebar():
+    ui.input_selectize("binsize", "Select Bin Size", 
+                    choices= [50, 500, 'gene', 'gene - split spike']),
+
+
