8bca9118fb4f8acbf9aab3d424cb2551340d9a38
Erin Gill <erin.gill81@gmail.com>
2024-03-19 16:09:19 -0700

    feat: add tabs

--- covid-mutation-distribution/app.py
+++ covid-mutation-distribution/app.py
@@ -1,98 +1,125 @@
+from functools import partial
 from shiny.express import input, render, ui
 from shiny import reactive
+from shiny.ui import page_navbar
 import plotly.graph_objects as go
 from shinywidgets import render_widget
 import functions
 
-ui.page_opts(title="SARS-CoV-2 Chronic Infection Calculator")
+ui.page_opts(
+    title="SARS-CoV-2 Chronic Infection Calculator",
+    page_fn=partial(page_navbar, id="page"),
+    fillable=True
+)
 
-with ui.sidebar(bg="#f8f8f8"):
-    ui.input_select("var", "Select Bin Size", 
+
+with ui.nav_panel("Home"):
+
+    with ui.layout_columns(col_widths=(4, 8)):
+        with ui.card():
+            ui.input_select("var", "Select Bin Size", 
                     choices= [int(500), int(1000), 'gene', 'genes_split'])
-    (ui.input_text_area("var2", "Please enter a comma-separated list of nucleotide positions where mutations occur here", ""),)
-    ui.input_select("var3", "Select Palette",
+            (ui.input_text_area("var2", "Please enter a comma-separated list of nucleotide positions where mutations occur here", ""),)
+            ui.input_select("var3", "Select Palette",
                     choices= ["viridis", "inferno", "plasma"])
 
-chronic, total_chronic = functions.parse_mutation_files('covid-mutation-distribution/chronicnucl.tsv')
-deer, total_deer = functions.parse_mutation_files('covid-mutation-distribution/deernucl.tsv')
-global_, total_global = functions.parse_mutation_files('covid-mutation-distribution/globalnucl.tsv')
+        with ui.card():
+            chronic, total_chronic = functions.parse_mutation_files('covid-mutation-distribution/chronicnucl.tsv')
+            deer, total_deer = functions.parse_mutation_files('covid-mutation-distribution/deernucl.tsv')
+            global_, total_global = functions.parse_mutation_files('covid-mutation-distribution/globalnucl.tsv')
 
 
-@render_widget
-def hist1():
-    x0 = chronic
-    x1 = global_
-    x2 = deer
+            @render_widget
+            def hist1():
+                x0 = chronic
+                x1 = global_
+                x2 = deer
 
-    counts0, bins0 = functions.make_bins(x0,input.var())
-    counts1, bins1 = functions.make_bins(x1,input.var())
-    counts2, bins2 = functions.make_bins(x2,input.var())
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-    x=bins0,
-    y=[x/total_chronic for x in counts0], # normalize bin counts by total number of mutations
-    name='chronic', # name used in legend and hover labels,
-    marker_color=functions.select_palette(input.var3())[0],
-    opacity=0.75
-    ))
-    fig.add_trace(go.Bar(
-    x=bins0,
-    y=[x/total_global for x in counts1], # normalize bin counts by total number of mutations
-    name='global', # name used in legend and hover labels,
-    marker_color=functions.select_palette(input.var3())[1],
-    opacity=0.75
-    ))
+                counts0, bins0 = functions.make_bins(x0,input.var())
+                counts1, bins1 = functions.make_bins(x1,input.var())
+                counts2, bins2 = functions.make_bins(x2,input.var())
+                fig = go.Figure()
+                fig.add_trace(go.Bar(
+                x=bins0,
+                y=[x/total_chronic for x in counts0], # normalize bin counts by total number of mutations
+                name='chronic', # name used in legend and hover labels,
+                marker_color=functions.select_palette(input.var3())[0],
+                opacity=0.75
+                ))
+                fig.add_trace(go.Bar(
+                x=bins0,
+                y=[x/total_global for x in counts1], # normalize bin counts by total number of mutations
+                name='global', # name used in legend and hover labels,
+                marker_color=functions.select_palette(input.var3())[1],
+                opacity=0.75
+                ))
 
-    fig.add_trace(go.Bar(
-    x=bins0,
-    y=[x/total_deer for x in counts2], # normalize bin counts by total number of mutations
-    name='deer', # name used in legend and hover labels,
-    marker_color=functions.select_palette(input.var3())[2],
-    opacity=0.75
-    ))
-    fig.update_layout(
-    title_text='Distribution of Mutations\nAcross Genome', # title of plot
-    xaxis_title_text='Genome Position', # xaxis label
-    yaxis_title_text='Proportion of Mutations', # yaxis label
-    bargap=0.2, # gap between bars of adjacent location coordinates
-    bargroupgap=0.1 # gap between bars of the same location coordinates
+                fig.add_trace(go.Bar(
+                x=bins0,
+                y=[x/total_deer for x in counts2], # normalize bin counts by total number of mutations
+                name='deer', # name used in legend and hover labels,
+                marker_color=functions.select_palette(input.var3())[2],
+                opacity=0.75
+                ))
+                fig.update_layout(
+                title_text='Distribution of Mutations\nAcross Genome', # title of plot
+                xaxis_title_text='Genome Position', # xaxis label
+                yaxis_title_text='Proportion of Mutations', # yaxis label
+                bargap=0.2, # gap between bars of adjacent location coordinates
+                bargroupgap=0.1 # gap between bars of the same location coordinates
+                )
+                return fig
+            
+            @reactive.calc
+            def calc_likelihoods():
+                likelihood_list, most_likely = functions.most_likely(input.var(), global_, chronic, deer, input.var2())
+                return likelihood_list, most_likely
+
+            @render.text
+            def txt():
+                return f'The log likelihoods of your sequence fitting the mutation distributions above are as follows:'
+
+            @render.text
+            def txt1():
+                try:
+                    return f'{calc_likelihoods()[0][0][1]}: {calc_likelihoods()[0][0][0]}'
+                except:
+                    return f'Please enter a comma-separated list of integer nucleotide positions in the box on the left to see your results.'
+            
+
+            @render.text
+            def txt2():
+                try:
+                    return f'{calc_likelihoods()[0][1][1]}: {calc_likelihoods()[0][1][0]}'
+                except:
+                    pass
+
+            @render.text
+            def txt3():
+                try:
+                    return f'{calc_likelihoods()[0][2][1]}: {calc_likelihoods()[0][2][0]}'
+                except:
+                    pass
+
+            @render.text
+            def txt4():
+                try:
+                    return f'Your sequence best fits the distribution of {calc_likelihoods()[1][1]} mutations.'
+                except:
+                    pass
+
+with ui.nav_panel("Application Notes"):
+    ui.markdown(
+        '''
+        # Overview
+        This application was developed by the Computational Analysis, Modelling and Evolutionary Outcomes 
+        ([CAMEO](https://covarrnet.ca/computational-analysis-modelling-and-evolutionary-outcomes-cameo/)) 
+        pillar of Canada's Coronavirus Variants Rapid Response Network ([CoVaRR-Net](https://covarrnet.ca/)). 
+        Data analysis, code and maintenance of the application are conducted by Erin E. Gill, Fiona S.L. Brinkman,
+        and Sarah Otto. 
+        
+        # Background
+        This application accepts a list of genomic positions where 
+        
+        '''
     )
-    return fig
-    
-@reactive.calc
-def calc_likelihoods():
-    likelihood_list, most_likely = functions.most_likely(input.var(), global_, chronic, deer, input.var2())
-    return likelihood_list, most_likely
-
-@render.text
-def txt():
-    return f'The log likelihoods of your sequence fitting the mutation distributions above are as follows:'
-
-@render.text
-def txt1():
-    try:
-        return f'{calc_likelihoods()[0][0][1]}: {calc_likelihoods()[0][0][0]}'
-    except:
-        return f'Please enter a comma-separated list of integer nucleotide positions in the box on the left to see your results.'
-    
-
-@render.text
-def txt2():
-    try:
-        return f'{calc_likelihoods()[0][1][1]}: {calc_likelihoods()[0][1][0]}'
-    except:
-        pass
-
-@render.text
-def txt3():
-    try:
-        return f'{calc_likelihoods()[0][2][1]}: {calc_likelihoods()[0][2][0]}'
-    except:
-        pass
-
-@render.text
-def txt4():
-    try:
-        return f'Your sequence best fits the distribution of {calc_likelihoods()[1][1]} mutations.'
-    except:
-        pass
--- covid-mutation-distribution/functions.py
+++ covid-mutation-distribution/functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 
 example_mutation_list = [897, 3431, 7842, 8293, 8393, 11042, 12789, 13339, 15756, 18492, 21608, 21711, 21941, 22032, 22208, 22034, 22295, 22353, 22556, 22770, 22895, 22896, 22898, 22910, 22916, 23009, 23012, 23013, 23018, 23019, 23271, 23423, 23604, 24378, 24990, 25207, 26529, 26610, 26681, 26833, 28958]
 
@@ -51,29 +52,32 @@ def get_likelihood(existing_bin_counts, test_bin_counts):
 
 # function to determine most likely distribution
 def most_likely(binsize, global_, chronic, deer, mutated_nucleotide_list):
+    mutated_nucleotide_list = mutated_nucleotide_list.split(',')
     try: 
-        mutated_nucleotide_list = mutated_nucleotide_list.split(',')
         mut_nuc_list = [int(i) for i in mutated_nucleotide_list]
-        mut_counts, mut_bins = make_bins(mut_nuc_list, binsize)
-        # get bins for global, chronic and deer
-        global_counts, global_bins = make_bins(global_,binsize)
-        chronic_counts, chronic_bins = make_bins(chronic,binsize)
-        deer_counts, deer_bins = make_bins(deer,binsize)
-    
-        # calculate all likelihoods
-        global_likelihood = get_likelihood(global_counts, mut_counts)
-        chronic_likelihood = get_likelihood(chronic_counts, mut_counts)
-        deer_likelihood = get_likelihood(deer_counts, mut_counts)
-    
-        # make a list of all likelihoods, find most likely
-        likelihood_list = [global_likelihood, chronic_likelihood, deer_likelihood]
-        names = ['global', 'chronic', 'deer']
-        zipped = list(zip(likelihood_list, names))
-        best_fit = max(zipped)
-        return zipped, best_fit
     except:
+        int_nuc_list = [re.sub('\D', '', i) for i in mutated_nucleotide_list]
+        mut_nuc_list = [int(i) for i in int_nuc_list]
+    else:
         pass
+    mut_counts, mut_bins = make_bins(mut_nuc_list, binsize)
+    # get bins for global, chronic and deer
+    global_counts, global_bins = make_bins(global_,binsize)
+    chronic_counts, chronic_bins = make_bins(chronic,binsize)
+    deer_counts, deer_bins = make_bins(deer,binsize)
     
+    # calculate all likelihoods
+    global_likelihood = get_likelihood(global_counts, mut_counts)
+    chronic_likelihood = get_likelihood(chronic_counts, mut_counts)
+    deer_likelihood = get_likelihood(deer_counts, mut_counts)
+    
+    # make a list of all likelihoods, find most likely
+    likelihood_list = [global_likelihood, chronic_likelihood, deer_likelihood]
+    names = ['global', 'chronic', 'deer']
+    zipped = list(zip(likelihood_list, names))
+    best_fit = max(zipped)
+    return zipped, best_fit
+
 # function to select palettes
 def select_palette(palette_name):
     if palette_name == "viridis":
